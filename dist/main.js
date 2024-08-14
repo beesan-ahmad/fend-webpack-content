874349d893bd0c18b87c42f0bdcedd191848b552
beesan-ahmad <abumfarreh@gmail.com>
2024-08-14 20:27:49 +0300

    add prod and dev mode for webpack

--- dist/main.js
+++ dist/main.js
@@ -1,24 +1,5 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/client/index.js":
-/*!*****************************!*\
-  !*** ./src/client/index.js ***!
-  \*****************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_nameChecker__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/nameChecker */ \"./src/client/js/nameChecker.js\");\n/* harmony import */ var _js_nameChecker__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_nameChecker__WEBPACK_IMPORTED_MODULE_0__);\n// babel loader take our ES6 syntax and turning it into normal js for our browser.\n\nconsole.log(_js_nameChecker__WEBPACK_IMPORTED_MODULE_0__.checkForName);\nalert('hi!!');\n\n//# sourceURL=webpack://example-project/./src/client/index.js?");
-
-/***/ }),
 
 /***/ "./src/client/js/nameChecker.js":
 /*!**************************************!*\
@@ -26,36 +7,47 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_
   \**************************************/
 /***/ (() => {
 
-eval("function checkForName(inputText) {\n  console.log(\"::: Running checkForName :::\", inputText);\n  var names = [\"Picard\", \"Janeway\", \"Kirk\", \"Archer\", \"Georgiou\"];\n  if (names.includes(inputText)) {\n    alert(\"Welcome, Captain!\");\n  }\n}\n\n//# sourceURL=webpack://example-project/./src/client/js/nameChecker.js?");
+        function checkForName(inputText) {
+          console.log("::: Running checkForName :::", inputText);
+          var names = ["Picard", "Janeway", "Kirk", "Archer", "Georgiou"];
+          if (names.includes(inputText)) {
+            alert("Welcome, Captain!");
+          }
+        }
 
-/***/ })
+        /***/
+})
 
-/******/ 	});
+    /******/
+});
 /************************************************************************/
 /******/ 	// The module cache
 /******/ 	var __webpack_module_cache__ = {};
-/******/ 	
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
 /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
 /******/ 		if (cachedModule !== undefined) {
 /******/ 			return cachedModule.exports;
-/******/ 		}
+      /******/
+}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
 /******/ 			// no module.id needed
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
-/******/ 		};
-/******/ 	
+      /******/
+};
+/******/
 /******/ 		// Execute the module function
 /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
-/******/ 	}
-/******/ 	
+    /******/
+}
+/******/
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
@@ -66,43 +58,65 @@ eval("function checkForName(inputText) {\n  console.log(\"::: Running checkForNa
 /******/ 				() => (module);
 /******/ 			__webpack_require__.d(getter, { a: getter });
 /******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
+      /******/
+};
+    /******/
+})();
+/******/
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
 /******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 			for (var key in definition) {
+/******/ 				if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
 /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
+          /******/
+}
+        /******/
+}
+      /******/
+};
+    /******/
+})();
+/******/
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
+    /******/
+})();
+/******/
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
 /******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
+        /******/
+}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/client/index.js");
-/******/ 	
-/******/ })()
-;
+      /******/
+};
+    /******/
+})();
+  /******/
+  /************************************************************************/
+  var __webpack_exports__ = {};
+  // This entry need to be wrapped in an IIFE because it need to be in strict mode.
+  (() => {
+    "use strict";
+    /*!*****************************!*\
+      !*** ./src/client/index.js ***!
+      \*****************************/
+    __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_nameChecker__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/nameChecker */ "./src/client/js/nameChecker.js");
+/* harmony import */ var _js_nameChecker__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_nameChecker__WEBPACK_IMPORTED_MODULE_0__);
+    // babel loader take our ES6 syntax and turning it into normal js for our browser.
+
+    console.log(_js_nameChecker__WEBPACK_IMPORTED_MODULE_0__.checkForName);
+    alert('hi!!');
+  })();
+
+  /******/
+})()
+  ;
+//# sourceMappingURL=main.js.map
